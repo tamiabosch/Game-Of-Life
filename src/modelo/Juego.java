e7c25af2b982ee3a0b2ba4546fd2d8fecb7fb9e3
tamiabosch <tamiabosch@yahoo.de>
2017-11-03 21:40:56 +0100

    added Exception Hanlding to the constructor, cargaPatron() and actualiza()

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
+import modelo.excepciones.ExcepcionArgumentosIncorrectos;
+import modelo.excepciones.ExcepcionCoordenadaIncorrecta;
+import modelo.excepciones.ExcepcionEjecucion;
+import modelo.excepciones.ExcepcionPosicionFueraTablero;
+
 /**
  * Class Juego with a tablero and reglaConway
  * @author Tami
@@ -30,41 +35,63 @@ public class Juego {
 	 * @param regla
 	 */
 	public Juego(Tablero tablero, ReglaConway regla) {
-		this.tablero = tablero;
-		this.regla = regla;
+		if(tablero != null && regla != null) {
+			this.tablero = tablero;
+			this.regla = regla;
+		} else {
+			throw new ExcepcionArgumentosIncorrectos("The tablero or regla is null in the Juego class");
+		}
 	}
 	
 	/**
 	 * Tries to load the pattern and store it on patronesUsados
 	 * @param p Patron
 	 * @param posicionInicial Coordenada
+	 * @throws ExcepcionPosicionFueraTablero 
 	 */
-	public void cargaPatron(Patron p, Coordenada posicionInicial) {
-		if(tablero.cargaPatron(p, posicionInicial)) {
-			patronesUsados.add(p);
+	public void cargaPatron(Patron p, Coordenada posicionInicial) throws ExcepcionPosicionFueraTablero {
+		if(p != null && posicionInicial != null) {
+			//altes if statment tablero.cargaPatron(p, posicionInicial)
+			//Values to check of the patron fits into the tablero
+			Coordenada lastCoordenada = tablero.getDimensiones();
+			int xSumaCoordenada = posicionInicial.suma(lastCoordenada).getX();
+			int ySumaCoordenada = posicionInicial.suma(lastCoordenada).getY();
+			int xCoordenadaDim = tablero.getDimensiones().getX();		//wahrscheinlich dim des juegos benutzten!!!
+			int yCoordenadaDim = tablero.getDimensiones().getY();
+			if(posicionInicial.getX() >= 0 || posicionInicial.getY() >= 0 || (xSumaCoordenada  <= xCoordenadaDim && ySumaCoordenada <= yCoordenadaDim)) {
+				patronesUsados.add(p);
 			
-		
+			} else {
+				throw new ExcepcionPosicionFueraTablero(tablero.getDimensiones(), posicionInicial);
+				//System.err.println("Error cargando plantilla " + p.getNombre() + " en (" + tablero.getNotFittingC().getX() + "," + tablero.getNotFittingC().getY() + ")");
+			}
 		} else {
-			System.err.println("Error cargando plantilla " + p.getNombre() + " en (" + tablero.getNotFittingC().getX() + "," + tablero.getNotFittingC().getY() + ")");
+			throw new ExcepcionArgumentosIncorrectos("Patron or CoordenadaInicial is null in Juego!");
 		}
 	}
 	
 	/**
 	 * calcuates the new state of the juego and all it's celdas
+	 * @throws ExcepcionCoordenadaIncorrecta 
 	 */
-	public void actualiza() {
-		HashMap<Coordenada, EstadoCelda> evolution = new HashMap<Coordenada, EstadoCelda>();
-		for( int i = 0; i < tablero.getDimensiones().getX(); i++) {
-			for( int j = 0; j < tablero.getDimensiones().getY(); j++ ) {
-				Coordenada newCoordenada = new Coordenada (i,j);
-				EstadoCelda newEstado = regla.calculaSiguienteEstadoCelda(tablero, newCoordenada);
-				evolution.put(newCoordenada, newEstado);
+	public void actualiza() throws ExcepcionCoordenadaIncorrecta {
+		try {
+			HashMap<Coordenada, EstadoCelda> evolution = new HashMap<Coordenada, EstadoCelda>();
+			for( int i = 0; i < tablero.getDimensiones().getX(); i++) {
+				for( int j = 0; j < tablero.getDimensiones().getY(); j++ ) {
+					Coordenada newCoordenada = new Coordenada (i,j);
+					EstadoCelda newEstado = regla.calculaSiguienteEstadoCelda(tablero, newCoordenada);
+					evolution.put(newCoordenada, newEstado);
+				}
 			}
+			Collection<Coordenada> collection = tablero.getPosiciones();
+			for(int i=0; i < collection.size(); i++) {
+				tablero.setCelda((Coordenada) collection.toArray()[i], evolution.get(collection.toArray()[i]));
+			}
+		} catch (ExcepcionPosicionFueraTablero e) {
+			throw new ExcepcionEjecucion(e);
 		}
-		Collection<Coordenada> collection = tablero.getPosiciones();
-		for(int i=0; i < collection.size(); i++) {
-			tablero.setCelda((Coordenada) collection.toArray()[i], evolution.get(collection.toArray()[i]));
-		}
+
 		
 	}
 
