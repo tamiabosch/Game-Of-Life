97275ded8d3b3a87a9ea866a5da0807e2e5f229a
tamiabosch <tamiabosch@yahoo.de>
2017-11-03 20:53:24 +0100

    added Exception Handling to toString() and getCelda()

--- src/modelo/Patron.java
+++ src/modelo/Patron.java
@@ -2,6 +2,11 @@ package modelo;
 
 import java.util.Collection;
 
+import modelo.excepciones.ExcepcionArgumentosIncorrectos;
+import modelo.excepciones.ExcepcionCoordenadaIncorrecta;
+import modelo.excepciones.ExcepcionEjecucion;
+import modelo.excepciones.ExcepcionPosicionFueraTablero;
+
 /**
  * creates a pattern which you can put into the juego
  * @author Tamia Bosch
@@ -32,40 +37,58 @@ public class Patron {
 	 */
 	@Override
 	public String toString() {
-		String result = "";
-		int sizeX = tablero.getDimensiones().getX();
-		int sizeY = tablero.getDimensiones().getY();
-		for(int j = 0; j <= sizeY+1; j++) {
-			for (int i = 0; i <= sizeX+1; i++) {
-				if (i==0 && j == 0 || i == sizeX+1 && j == 0 || i == 0 && j == sizeY+1 || i == sizeX+1 && j == sizeY+1) {
-					result += "+"; 
-					if (i == sizeX+1){
-					result += "\n";	
+		try {
+			String result = "";
+			int sizeX = tablero.getDimensiones().getX();
+			int sizeY = tablero.getDimensiones().getY();
+			for(int j = 0; j <= sizeY+1; j++) {
+				for (int i = 0; i <= sizeX+1; i++) {
+					if (i==0 && j == 0 || i == sizeX+1 && j == 0 || i == 0 && j == sizeY+1 || i == sizeX+1 && j == sizeY+1) {
+						result += "+"; 
+						if (i == sizeX+1){
+						result += "\n";	
+						}
+					} else if (j == 0 || j == sizeY+1) {
+						result += "-";
+					}else if (i == 0 || i == sizeX+1) {
+						result += "|";
+						if (i == sizeX+1) {
+							result += "\n";
+						}
+					} else if (this.getCelda(new Coordenada(i-1,j-1)) == EstadoCelda.MUERTA) {
+						result += " ";
+					}else if (this.getCelda(new Coordenada(i-1,j-1)) == EstadoCelda.VIVA) {
+						result += "*";
 					}
-				} else if (j == 0 || j == sizeY+1) {
-					result += "-";
-				}else if (i == 0 || i == sizeX+1) {
-					result += "|";
-					if (i == sizeX+1) {
-						result += "\n";
-					}
-				} else if (this.getCelda(new Coordenada(i-1,j-1)) == EstadoCelda.MUERTA) {
-					result += " ";
-				}else if (this.getCelda(new Coordenada(i-1,j-1)) == EstadoCelda.VIVA) {
-					result += "*";
 				}
 			}
+			return nombre + "\n" + result;
+			
+		} catch(ExcepcionCoordenadaIncorrecta e) {
+			throw new ExcepcionEjecucion(e);
+		} catch(ExcepcionArgumentosIncorrectos e) {
+			throw new ExcepcionEjecucion(e);
+		} catch(ExcepcionPosicionFueraTablero e) {
+			throw new ExcepcionEjecucion(e);
 		}
-		return nombre + "\n" + result;
+		
 	}
 
 	/**
 	 * getter for the status of cells
 	 * @param posicion
 	 * @return Estado of the celdas
+	 * @throws ExcepcionPosicionFueraTablero 
 	 */
-	public EstadoCelda getCelda(Coordenada posicion) {
-		return this.tablero.getCelda(posicion);
+	public EstadoCelda getCelda(Coordenada posicion) throws ExcepcionPosicionFueraTablero {
+		if(posicion != null) {
+			if(!tablero.contiene(posicion)) {
+				throw new ExcepcionPosicionFueraTablero(tablero.getDimensiones(), posicion);
+			} else {
+				return this.tablero.getCelda(posicion);			}
+		} else {
+			throw new ExcepcionArgumentosIncorrectos("Coordenada posicion is null!");
+		}
 	}
 	
 	/**
